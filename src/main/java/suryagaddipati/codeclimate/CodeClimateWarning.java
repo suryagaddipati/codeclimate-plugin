3ec340331200a1bcae8db6cde52340616a9b016e
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-12-15 07:16:14 -0600

    ignore unknow props

--- src/main/java/suryagaddipati/codeclimate/CodeClimateWarning.java
+++ src/main/java/suryagaddipati/codeclimate/CodeClimateWarning.java
@@ -1,5 +1,7 @@
 package suryagaddipati.codeclimate;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /*
 {"type":"Issue","check_name":"Rubocop/Style/SpaceBeforeComma","description":"Space found before comma.",
 "categories":["Style"],"remediation_points":50000,
@@ -7,6 +9,7 @@ package suryagaddipati.codeclimate;
 "positions":{"begin":{"column":38,"line":7},"end":{"column":39,"line":7}}},
 "content":{"body":"Checks for comma (,) preceded by space."},"engine_name":"rubocop"}
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CodeClimateWarning {
     public String type;
     public String check_name;
